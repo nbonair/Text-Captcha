0f4fdf3631900dd340feda3361b83de9a45710fa
nbonair <86250240+nbonair@users.noreply.github.com>
2022-12-13 19:51:51 +1100

    Add files via upload

--- Image Preprocessing/raw_data_process.py
+++ Image Preprocessing/raw_data_process.py
@@ -8,18 +8,11 @@ def raw_data_process(path):
     # Output: 
     #   images: list of dir to captcha images
     #   labels: list of corresponding labels
-    #   characters: list of possible characters in labels (here we includes all characters in alphabet and numbers)
     #   max_length: maximum length of a label
     path = Path(path)
     images = sorted(list(map(str, list(path.glob("*.png")))))
     #split labels
     labels = [img.split(os.path.sep)[-1].split(".png")[0] for img in images]
-
-    #Get dataset information
-    char_list = string.ascii_letters+string.digits
-
-    characters = set(char for char in char_list)
-    characters = sorted(list(characters))
     max_length = max([len(label) for label in labels])
     for i, label in enumerate(labels):
         if len(label) > 5:
@@ -28,8 +21,7 @@ def raw_data_process(path):
 
     print("Number of images found: ", len(images))
     print("Number of labels found: ", len(labels))
-    print("Number of unique characters: ", len(characters))
-    print("Characters present: ", characters)
+
     print("Maximum label length: ",max_length)
-    return images, labels, characters, max_length
+    return images, labels, max_length
 
